983189dbda7f2b046b5764b3a996edba4dd4c92d
Angelina Saliling <ajs649@cornell.edu>
2020-06-03 13:50:45 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+// set up cors to allow cross-origin requests
+const cors = require('cors')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/verve-api');
+
+// Initialize http server
+const app = express()
+
+// to allow parsing of request bodies
+// set up bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use('/api/users/', require('./routing/user'))
+
+const port = process.env.PORT || 3000;
+// creates express server
+app.listen(port, () => {
+  console.log('listening on 3000')
+})
